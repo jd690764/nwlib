d1fdcd87e43a92682d54b4695e7e3b7f2c37c19a
Janos Demeter <jd690764@gmail.com>
2017-05-18 12:15:06 -0700

    adding lookupPtmForm - JD

--- forms.py
+++ forms.py
@@ -2,12 +2,32 @@ from django import forms
 from numpy import unique
 import os
 from django.contrib.admin.filters import AllValuesFieldListFilter
+from network.models import Sample
+
+modifs = (('Propionamide', 'Propionamide'),
+          ('Oxidation', 'Oxidation'),
+          ('Double Oxidation', 'Double Oxidation'),
+          ('Deamidated', 'Deamidated'),
+          ('Gln->pyro-Glu', 'Gln->pyro-Glu'),
+          ('Glu->pyro-Glu', 'Glu->pyro-Glu'),
+          ('GlyGly(Ubiquitylation)', 'GlyGly(Ubiquitylation)'),
+          ('Carbamoylation', 'Carbamoylation'),
+          ('Methyl', 'Methyl'),
+          ('Acetyl', 'Acetyl'),
+          ('Phosphorylation', 'Phosphorylation'),
+          ('Biotinylation', 'Biotinylation'),
+          ('APEX2-Biotinylation','APEX2-Biotinylation'),
+          ('Unknown', 'Unknown'),)
+
+organisms = (  
+    ('9606', 'Human'),
+    ('10090', 'Mouse'),
+    ('all', 'all'),
+)
 
 
 class lookupForm(forms.Form):
 
-    #symbol     = forms.TextInput(attrs={'placeholder': 'KRAS', 'label': 'Symbol:'})
-    #symbol     = forms.CharField(required=True)
     symbol     = forms.CharField(label    = 'Symbol',
                                  required = True,
                                  widget   = forms.TextInput( attrs={'placeholder': 'KRAS'}))
@@ -18,13 +38,8 @@ class lookupForm(forms.Form):
 
 
     def __init__(self, *args, **kwargs):
-        super(lookupForm, self).__init__(*args, **kwargs)
 
-        organisms = (  
-            ('9606', 'Human'),
-            ('10090', 'Mouse'),
-            ('all', 'all'),
-        )
+        super(lookupForm, self).__init__(*args, **kwargs)
 
         ifilenames = [fn for fn in os.listdir('/mnt/msrepo/ifiles') if fn[-2:] == '.i' and not 'bioplex' in fn and fn[0] != '.' ]
         bs         = sorted(unique( [ fn.split('_')[0].upper() for fn in ifilenames ]))
@@ -37,6 +52,43 @@ class lookupForm(forms.Form):
         self.fields['org'].choices = organisms
         self.fields['bait'].choices = baits
 
+class lookupPtmForm(forms.Form):
+
+    symbol     = forms.CharField(label    = 'Symbol',
+                                 required = True,
+                                 widget   = forms.TextInput( attrs={'placeholder': 'KRAS'}))
+    org        = forms.ChoiceField(choices  = (),
+                                   required = True )
+    bait       = forms.MultipleChoiceField( widget  = forms.SelectMultiple( attrs = {'size':'20'}),
+                                            choices = ())
+    modif      = forms.ChoiceField(choices  = (),
+                                   required = True )
+    expt       = forms.ChoiceField(choices  = (),
+                                   required = True )
+
+    def __init__(self, *args, **kwargs):
+
+        super(lookupPtmForm, self).__init__(*args, **kwargs)
+
+        dirs       = os.listdir('/mnt/msrepo/fractionFiles')
+        samples    = Sample.objects.filter(ff_folder__in=dirs).values( 'label', 'bait_symbol' )
+        bs         = ['all'] + sorted(unique( [ k['bait_symbol'].upper() for k in samples ]))
+        baits      = list()
+        for b in bs:
+            baits.append((b, b))
+        baits      = tuple(baits)
+        ls         = ['all'] + sorted(unique( [ k['label'].upper() for k in samples ]))
+        labels     = list()
+        for l in ls:
+            labels.append((l, l))
+        labels     = tuple(labels)        
+
+        self.fields['org'].choices   = organisms
+        self.fields['bait'].choices  = baits
+        self.fields['expt'].choices  = labels
+        self.fields['modif'].choices = sorted(modifs)
+        
 
 class DropdownFilter(AllValuesFieldListFilter):
     template = 'admin/dropdown_filter.html'
+
