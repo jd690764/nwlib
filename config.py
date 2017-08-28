ddf08b13ef7f6e337a3a6900c6159d39e6a1e716
Janos Demeter <jd690764@gmail.com>
2017-08-28 15:04:06 -0700

    add a few new paths -JD

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ cddIdTableFtp     = 'ftp://ftp.ncbi.nih.gov/pub/mmdb/cdd/cddid.tbl.gz'
 
 uniprotUrl        = 'http://www.uniprot.org/uniprot/'
 
+BLASTDB           = '/mnt/driveC/blastdb'
 
 controlFiles      = '/mnt/reference/control_panels/'
 
@@ -14,6 +15,7 @@ rawfilesPath      = '/srv/msrepo/rawfiles/'
 mrmsfilesPath     = '/srv/msrepo/mrmsfiles/'
 properNounsPath   = '/home/mrkelly/Lab/errata/propers.txt'
 bgfilesPath       = '/srv/msrepo/background/'
+ptmsPath          = '/mnt/msrepo/fractionFiles/PTMs/'
 referencePath     = '/usr/local/share/py/djscripts/data/pickles/'
 
 bgerfilePath      = 'data/preprocess/backgrounder.tsv'
@@ -33,13 +35,16 @@ preppiPath        = publicDataPath + 'preppi_150727_lr600.i'
 domainPairsPath   = publicDataPath + 'bioplex_predicted.tsv'
 
 
-filesDict         = { 'hsg' : referencePath + 'hsg_latest' ,
-                      'mmg' : referencePath + 'mmg_latest' ,
-                      'hmg' : referencePath + 'hsmmg_latest' ,
-                      'hsp' : referencePath + 'hsp_latest' ,
-                      'mmp' : referencePath + 'mmp_latest' ,
-                      'cdd' : referencePath + 'cdd_latest' ,
-                      'h2m' : referencePath + 'h2m_latest' ,
-                      'm2h' : referencePath + 'm2h_latest' ,
-                      'dup' : referencePath + 'dup_latest' ,
+filesDict         = { 'hsg'    : referencePath + 'hsg_latest' ,
+                      'mmg'    : referencePath + 'mmg_latest' ,
+                      'hmg'    : referencePath + 'hsmmg_latest' ,
+                      'hsp'    : referencePath + 'hsp_latest' ,
+                      'mmp'    : referencePath + 'mmp_latest' ,
+                      'cdd'    : referencePath + 'cdd_latest' ,
+                      'h2m'    : referencePath + 'h2m_latest' ,
+                      'm2h'    : referencePath + 'm2h_latest' ,
+                      'h2ms'   : referencePath + 'h2m_symb_latest' ,
+                      'm2hs'   : referencePath + 'm2h_symb_latest' ,
+                      'dup'    : referencePath + 'dup_latest' ,
+                      'duptxt' : referencePath + 'dup_latest.txt' ,                      
 }
